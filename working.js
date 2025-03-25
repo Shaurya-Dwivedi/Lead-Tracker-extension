fc7abdd26059ffee8b4f020c6837e5ad544aa4f1
Shaurya-Dwivedi <shauryadwivedi2001@gmail.com>
2025-03-25 15:07:37 +0530

    Added Delete button and storage of links

--- working.js
+++ working.js
@@ -1,24 +1,32 @@
-let myLeads = []
+let myLeads= []
+let leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const inputEl = document.getElementById("input-el")
-const ulEl = document.querySelector("#ul-el");
+const ulEl = document.querySelector("#ul-el")
 const inputBtn = document.getElementById("input-btn")
+const delBtn=document.getElementById("del-btn")
+if(leadFromLocalStorage)
+{
+    myLeads = leadFromLocalStorage;
+    renderLeads();
+    
+}
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
+   
     inputEl.value=""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
     renderLeads();
-    // console.log(myLeads);
+    
 })
-
-for (let i = 0; i < myLeads.length; i++) {
-    const li = document.createElement("li");
-    li.textContent = myLeads[i];
-    ulEl.append(li);
-}
-
+delBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    renderLeads()
+})
 function renderLeads() {
     let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li>" + "<a href =' "+ myLeads[i] +"' target='_blank'> "+myLeads[i]+"</a> </li>";
         listItems+=`
         <li>
             <a target="_blank" href="${myLeads[i]}" >
