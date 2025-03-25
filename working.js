e9ce9f92ddc5703ce76d5d5bb2b60b7300d9c340
Shaurya-Dwivedi <shauryadwivedi2001@gmail.com>
2025-03-25 16:14:47 +0530

    Added save tab button

--- working.js
+++ working.js
@@ -1,39 +1,56 @@
-let myLeads= []
+let myLeads = []
 let leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const inputEl = document.getElementById("input-el")
 const ulEl = document.querySelector("#ul-el")
 const inputBtn = document.getElementById("input-btn")
-const delBtn=document.getElementById("del-btn")
-if(leadFromLocalStorage)
-{
+const tabBtn = document.getElementById("tab-btn")
+const delBtn = document.getElementById("del-btn")
+
+if (leadFromLocalStorage) {
     myLeads = leadFromLocalStorage;
-    renderLeads();
-    
+    render(myLeads);
+
 }
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value);
-   
-    inputEl.value=""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    
-    renderLeads();
-    
-})
-delBtn.addEventListener("dblclick",function(){
-    localStorage.clear()
-    myLeads=[]
-    renderLeads()
-})
-function renderLeads() {
+
+function render(data) {
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems+=`
+    for (let i = 0; i < data.length; i++) {
+        listItems += `
         <li>
-            <a target="_blank" href="${myLeads[i]}" >
-            ${myLeads[i]}
+            <a target="_blank" href="${data[i]}" >
+            ${data[i]}
             </a>
               </li>
         `
     }
     ulEl.innerHTML = listItems;
 }
+
+tabBtn.addEventListener("click", function () {
+
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    }
+    )
+}
+)
+
+
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads);
+
+})
+delBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
